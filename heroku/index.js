1f5c1a5b71d9ad7fc6f7022a524118959b5309e5
srsuper <aot5310@gmail.com>
2021-05-02 01:35:16 +0700

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
-app.get('/', function(req, res) {
+app.get('/Callback', function(req, res) {
   console.log(req);
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
